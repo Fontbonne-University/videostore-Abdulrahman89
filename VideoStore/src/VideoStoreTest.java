b5679cc61eaebc68d513c9f3bfd1cf022b2f5556
albis <albis@DESKTOP-756268P.attlocal.net>
2019-03-26 22:55:42 -0500

    VideoStor

--- VideoStore/src/VideoStoreTest.java
+++ VideoStore/src/VideoStoreTest.java
@@ -1,38 +1,70 @@
-import junit.framework.*;
 
-public class VideoStoreTest extends TestCase
+import org.junit.*;
+
+public class VideoStoreTest 
 {
-	public VideoStoreTest (String name) {
-		super (name);
-	}
-	
-	protected void setUp ()  {
-		customer = new Customer ("Fred");
-	}
-	
-	public void testSingleNewReleaseStatement () {
-		customer.addRental (new Rental (new Movie ("The Cell", Movie.NEW_RELEASE), 3));		
-		assertEquals ("Rental Record for Fred\n\tThe Cell\t9.0\nYou owed 9.0\nYou earned 2 frequent renter points\n", customer.statement ());
-	}
 
-	public void testDualNewReleaseStatement () {
-		customer.addRental (new Rental (new Movie ("The Cell", Movie.NEW_RELEASE), 3));
-		customer.addRental (new Rental (new Movie ("The Tigger Movie", Movie.NEW_RELEASE), 3));
-		assertEquals ("Rental Record for Fred\n\tThe Cell\t9.0\n\tThe Tigger Movie\t9.0\nYou owed 18.0\nYou earned 4 frequent renter points\n", customer.statement ());
-	}
-
-	public void testSingleChildrensStatement () {
-		customer.addRental (new Rental (new Movie ("The Tigger Movie", Movie.CHILDRENS), 3));
-		assertEquals ("Rental Record for Fred\n\tThe Tigger Movie\t1.5\nYou owed 1.5\nYou earned 1 frequent renter points\n", customer.statement ());
+	
+	private static final Movie regular3 = new RegularMovie ("Regular 3");
+	private static final Movie regular2 = new RegularMovie ("Regular 2");
+	private static final Movie regular1 = new RegularMovie ("Regular 1");
+	private static final Movie childrenMovie = new ChildrensMovie ("Childrens");
+	private static final Movie newReleaseMovie2 = new NewReleaseMovie ("New Release 2");
+	private static final Movie newReleaseMovie1 = new NewReleaseMovie ("New Release 1");
+	private static final double DELTA = .001;
+	private Statement statement;
+	
+	@Before
+	public void setUp ()  {
+		statement = new Statement ("Customer");
 	}
 	
-	public void testMultipleRegularStatement () {
-		customer.addRental (new Rental (new Movie ("Plan 9 from Outer Space", Movie.REGULAR), 1));
-		customer.addRental (new Rental (new Movie ("8 1/2", Movie.REGULAR), 2));
-		customer.addRental (new Rental (new Movie ("Eraserhead", Movie.REGULAR), 3));
+	@Test
+	public void testSingleNewReleaseStatementTotals () {
+		statement.addRental (new Rental (newReleaseMovie1, 3));		
+		statement.generate();
+		Assert.assertEquals(9.0, statement.getTotal(), DELTA);
+		Assert.assertEquals(2, statement.getFrequentRenterPoints());
+	}
+	@Test
+	public void testDualNewReleaseStatementTotals () {
+		statement.addRental (new Rental (newReleaseMovie1, 3));
+		statement.addRental (new Rental (newReleaseMovie2, 3));
+		statement.generate ();
+		Assert.assertEquals(18.0, statement.getTotal(), .001);
+		Assert.assertEquals(4, statement.getFrequentRenterPoints());
+	}
+	@Test
+	public void testSingleChildrensStatementTotals () {
+		statement.addRental (new Rental (childrenMovie, 3));
+		statement.generate ();
+		Assert.assertEquals(1.5, statement.getTotal(), .001);
+		Assert.assertEquals(1, statement.getFrequentRenterPoints());
+	}
+	@Test
+	public void testMultipleRegularStatementTotals () {
+		statement.addRental (new Rental (regular1, 1));
+		statement.addRental (new Rental (regular2, 2));
+		statement.addRental (new Rental (regular3, 3));
 		
-		assertEquals ("Rental Record for Fred\n\tPlan 9 from Outer Space\t2.0\n\t8 1/2\t2.0\n\tEraserhead\t3.5\nYou owed 7.5\nYou earned 3 frequent renter points\n", customer.statement ());
+		statement.generate ();
+		Assert.assertEquals(7.5, statement.getTotal(), .001);
+		Assert.assertEquals(3, statement.getFrequentRenterPoints());
+	}
+	@Test
+	public void testMultipleRegularStatementFormat () {
+		statement.addRental (new Rental (regular1, 1));
+		statement.addRental (new Rental (regular2, 2));
+		statement.addRental (new Rental (regular3, 3));
+		
+		Assert.assertEquals ("Rental Record for Customer\n"
+				+ "\tRegular 1\t2.0\n"
+				+ "\tRegular 2\t2.0\n"
+				+ "\tRegular 3\t3.5\n"
+				+ "You owed 7.5\n"
+				+ "You earned 3 frequent renter points\n", statement.generate ());
 	}
 
-	private Customer customer;
+
+	
 }
